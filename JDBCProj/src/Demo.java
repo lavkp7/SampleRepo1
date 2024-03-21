bb45fdd4c91eb293af3aef50b2889f5bd5f7dff1
lavan <lavan@192.168.0.72>
2024-03-20 21:55:10 -0500

    second commit
     Adding JDBC proj

---
+++ JDBCProj/src/Demo.java
@@ -0,0 +1,21 @@
+import java.sql.DriverManager;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//	try {
+		//	Class.forName("jdbc:mysql://localhost:3306/?user=root;password=Chitti@2023");
+		//	DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=root;password=Chitti@2023");
+			
+		//} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		//}
+		
+		
+
+	}
+
+}
