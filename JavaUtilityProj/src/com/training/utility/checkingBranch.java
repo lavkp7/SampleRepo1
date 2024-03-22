bdbd1d9278cf917714b41d59f877b26b74c60494
lavan <lavan@192.168.0.72>
2024-03-22 14:55:15 -0500

    addded a class in local/working directory

---
+++ JavaUtilityProj/src/com/training/utility/checkingBranch.java
@@ -0,0 +1,10 @@
+package com.training.utility;
+
+public class checkingBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("branch created");
+	}
+
+}
